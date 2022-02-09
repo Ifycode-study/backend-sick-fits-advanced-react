8359331d00ab86d1e9671f5b79b9cce103d5bf86
Ifycode <marychord@gmail.com>
2022-02-09 01:47:20 +0100

    Learning: Creating two way data relationships in keystone

--- schemas/ProductImage.ts
+++ schemas/ProductImage.ts
@@ -1,5 +1,5 @@
 import { list } from '@keystone-next/keystone/schema';
-import { text } from '@keystone-next/fields';
+import { relationship, text } from '@keystone-next/fields';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
 import 'dotenv/config';
 
@@ -17,5 +17,11 @@ export const ProductImage = list({
       label: 'Source',
     }),
     altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
   },
 });
